51a60d41cfc659cd43db2e1ef35ea736b9cc7c1b
OLUWASINA OkikiJesu <74295767+okikijesutech@users.noreply.github.com>
2024-08-07 14:20:36 +0100

    Multiple language setup

--- src/pages/signup/GetStarted.tsx
+++ src/pages/signup/GetStarted.tsx
@@ -6,56 +6,67 @@ const languages = [
     image: "tribelogo1",
     language: "Yoruba",
     subs: "21k subscribed",
+    link: "/learnlanguage",
   },
   {
     image: "tribelogo2",
     language: "Hausa",
     subs: "21k subscribed",
+    link: "/learnlanguage",
   },
   {
     image: "tribelogo3",
     language: "Igbo",
     subs: "21k subscribed",
+    link: "/learnlanguage",
   },
   {
     image: "tribelogo4",
     language: "Fulfulde (Fulani)",
     subs: "21k subscribed",
+    link: "/learnlanguage",
   },
   {
     image: "tribelogo5",
     language: "Kanuri",
     subs: "21k subscribed",
+    link: "/learnlanguage",
   },
   {
     image: "tribelogo6",
     language: "Ibibio",
     subs: "21k subscribed",
+    link: "/learnlanguage",
   },
   {
     image: "tribelogo7",
     language: "Tiv",
     subs: "21k subscribed",
+    link: "/learnlanguage",
   },
   {
     image: "tribelogo8",
     language: "Ijaw",
     subs: "21k subscribed",
+    link: "/learnlanguage",
   },
   {
     image: "tribelogo9",
     language: "Edo",
     subs: "21k subscribed",
+    link: "/learnlanguage",
   },
   {
     image: "tribelogo10",
     language: "Urhobo",
     subs: "21k subscribed",
+    link: "/learnlanguage",
   },
   {
     image: "tribelogo11",
     language: "Nupe",
     subs: "21k subscribed",
+    link: "/learnlanguage",
   },
 ];
 
@@ -70,7 +81,7 @@ const Signup = () => {
             language={language.language}
             image={language.image}
             subs={language.subs}
-            link='/'
+            link={language.link}
           />
         ))}
       </div>
