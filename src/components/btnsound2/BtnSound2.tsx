9bda92d69cb038cd5bba15df1e61372e8efedf86
OLUWASINA OkikiJesu <74295767+okikijesutech@users.noreply.github.com>
2024-08-27 17:05:41 +0100

    Display 2

--- src/components/btnsound2/BtnSound2.tsx
+++ src/components/btnsound2/BtnSound2.tsx
@@ -1,9 +1,19 @@
 import { AiFillSound } from "react-icons/ai";
 import "./btnsound2.css";
 
-const BtnSound2 = () => {
+interface BtnSound2Props {
+  rate?: number;
+  sound: string;
+}
+
+const BtnSound2: React.FC<BtnSound2Props> = ({ sound, rate }) => {
+  const playSound = () => {
+    const audio = new Audio(sound);
+    audio.playbackRate = rate;
+    audio.play();
+  };
   return (
-    <button className='btnSoundContainer'>
+    <button className='btnSoundContainer' onClick={playSound}>
       <AiFillSound size={36} color='#131f24' />
     </button>
   );
--- src/components/questiondisplay2/QuestionDisplay2.tsx
+++ src/components/questiondisplay2/QuestionDisplay2.tsx
@@ -1,5 +1,6 @@
 import BtnSound2 from "../btnsound2/BtnSound2";
 import BtnWords from "../btnwords/BtnWords";
+import sound from "../../assets/sounds/a.mp3";
 import "./questiondisplay2.css";
 
 interface Option {
@@ -31,13 +32,13 @@ const QuestionDisplay2: React.FC<QuestionDisplayProps> = ({
       </div>
       <div className='soundButtonsContainer'>
         <div className='bigBtn'>
-          <BtnSound2 />
+          <BtnSound2 sound={sound} rate={1} />
         </div>
         <div className='smallBtn'>
-          <BtnSound2 />
+          <BtnSound2 sound={sound} rate={0.5} />
         </div>
       </div>
-      <hr />
+      <hr className='displayHr' />
       <div className='optionsdisplay'>
         {question.options.map((option) => (
           <div
